9712412494fcb2a190169bdaa65b69375a29b77c
ifyoutryproeasy <ripmakthug2pac@gmail.com>
2026-05-16 17:26:17 +0800

    basic training

---
+++ lectures/lec01-computation/justtraining.py
@@ -0,0 +1,54 @@
+x = 45
+print (type(x)) # <class 'integer'>
+A = "a letter" # this is the string string must have""
+print (type(A))
+z = 4.657
+print (type(z))
+a = "124"
+print (type(a))
+age = 25
+print(type(age))
+student_name = "Someone"
+total_score = 100
+print(type(total_score))
+isStudent = True
+print(type(isStudent))
+x1 = 5
+print(type(x))
+_temp = 45
+print(type(_temp))
+print("I will now count my bills")
+print("Hens", 25+30/6)
+rememberthis="if, while, for, def, class, True, False, None, print, return, in, not, and, or..."
+print(f"Phytond variabled ashiglaj bolohgui:{rememberthis}")
+
+#энэ хэсэгт хувьсагчийн төрөлүүдийг солих болно
+x = 5
+text = str(x)
+print(text)
+print(type(x))
+print(type(text))
+
+age_text = "25"
+print(type(age_text))
+age = int(age_text)
+print(type(age))
+print(age + 5)
+
+number = 5
+f = float(number)
+print(f)
+print(f"f ashiglaj baina {f}")
+
+age = 55
+
+young_age = age >= 45 and age <65 # hoyulaa unen baih nuhtsul shalgahad and ashiglana
+print(young_age)
+
+care_age = age < 55 or age >= 88
+print(care_age)
+
+#haritsuulah uildel
+tentsuu = "=="
+print(f"{tentsuu}ene tentsuu gesen ug ! jishee ni 5 == 5")
+
